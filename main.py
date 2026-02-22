4375ac01d7d97f3d0eb3177635f3c421f88531d8
devPietroMatos <pietromatoscontato@gmail.com>
2026-02-22 18:37:28 -0300

    Is user an admin
    Analyses if the program is launched as an admin. If not, the program ask for permission.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from time import sleep
 import pygame
+import ctypes
+import sys
 import os
 
-while True:
-    sleep(67)
-    pygame.mixer.init()
-    pygame.mixer.music.load("audio.mp3")
-    pygame.mixer.music.play()
-    os.startfile("text.txt")
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas",sys.executable," ".join(sys.argv),None,1)
+    sys.exit()
+
+def say_six_seven():
+    while True:
+        sleep(67)
+        pygame.mixer.init() # Inicia o mixer do pygame
+        pygame.mixer.music.load("audio.mp3") # Carrega o áudio
+        pygame.mixer.music.play() # Toca o áudio
+        os.startfile("text.txt") # Mostra o bloco de notas
+
+say_six_seven()
