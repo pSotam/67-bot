6344611b575f8a1d4ec973adec916e433dd4e598
devPietroMatos <pietromatoscontato@gmail.com>
2026-02-22 18:23:49 -0300

    main function

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from time import sleep
+import pygame
 
+while True:
+    sleep(367)
+    pygame.mixer.init()
+    pygame.mixer.music.load("audio.mp3")
+    pygame.mixer.music.play()
+    print("67")
